a51d1ea1bb9f95accfe0205d4847fa9088aa88b5
Rajdeep Yadav <rajdeepyadav004@gmail.com>
2019-06-14 05:04:46 +0530

    cleaned the files a bit

--- nets.py
+++ nets.py
@@ -2,44 +2,61 @@ import xml.etree.ElementTree as ET
 import tkinter as tk
 
 
+
 class tdpn:
+
+
 	def __init__(self):
 		self.places=[]
+		self.data=[]
 		self.transitions=[]
-		self.arcs=[]
-		self.coordinates=dict()
 		self.initMarking=dict()
-		##arcs are of the format (type, place, transition, condition)
-	
+		self.finalMarking=dict()
+		##transitions are tuples (name, list of arcs coming, list of arc going)
+		## each arc is a tuple (place, type, constraint, identifier)
+
+
 	def addPlaces(self, listOfPlaces):
 		self.places = self.places + listOfPlaces
+
 	
 	def addTrans(self, listOfTrans):
 		self.transitions = self.transitions + listOfTrans
 
-	def addArcs(self, listOfArcs):
-		self.arcs = self.arcs + listOfArcs
 
-	def addCoordinates(self, dictOfCoordinates):
-		self.coordinates = {**self.coordinates, **dictOfCoordinates}
-
-	#assume marking is a dict with items as list of ages 
+	#assume marking is a dict with keys as places and items as list of ages 
 	def addInitMarking(self, marking):
 		self.initMarking = marking
 
-	def __str__(self):
-		val = "places: " + " ".join(self.places)+"\n"
-		val = val + "transitions: " + " ".join(self.transitions)+"\n"
-		val = val + "arcs: \n"
-		count = 0
-		for arc in self.arcs:
-			count += 1
-			val = val + "\t" + str(count) + ".\n"
-			val = val + "\t  type: " + arc[0] + "\n"
-			val = val + "\t  place: " + arc[1] + "\n"
-			val = val + "\t  transition: " + arc[2] + "\n" 
-			val = val + "\t  conditions: " + arc[3] + "\n"
 
+	def addFinalMarking(self, marking):
+		self.finalMarking = marking
+
+
+	def __str__(self):
+
+		#print places
+		val = "places: " + " ".join(self.places)+"\n"
+
+		#print Transitions
+		for trans in self.transitions:
+
+			val += "name: "+ trans[0] + "\n"
+
+			val += "in arcs: " + "\n"
+			for arc in trans[1]:
+				if arc[1]=="interval":
+					val+= "\t"+ " ".join(map(str,(arc[0],arc[1],"("+str(arc[2]) + ","+ str(arc[2]+1) +")", arc[3] )))+"\n"
+				else:
+					val += "\t" + " ".join(map(str, arc)) + "\n"
+
+			val += "out arcs: \n"
+			for arc in trans[2]:
+				val += "\t" + " ".join(map(str, arc))  + "\n"
+
+			val+="\n"
+
+		#print markings
 		val = val + "Initial Markings: " + "\n"
 		count = 0
 		for place in self.initMarking.keys():
@@ -48,101 +65,80 @@ class tdpn:
 
 		return val
 
-	def readFromCPN(self, fileName):
-		root = ET.parse(fileName).getroot()
-		places = root.findall("cpnet/page/place")
-		transitions = root.findall("cpnet/page/trans")
-		arcs = root.findall("cpnet/page/arc")
 
-		IDtoPlace = {place.attrib["id"]:place.find("text").text for place in places} 
-		IDtoTrans = {trans.attrib["id"]:trans.find("text").text for trans in transitions}
+	__repr__ = __str__
+	
+### END OF CLASS ###		
 
-		self.addPlaces([(place.find("text").text)  for place in places])
-		self.addTrans([(trans.find("text").text) for trans in transitions])
-		self.addArcs([(arc.attrib["orientation"], IDtoPlace[arc.find("placeend").attrib["idref"]], IDtoTrans[arc.find("transend").attrib["idref"]], arc.find("annot/text").text) for arc in arcs])
-
-	def display(self):
-		master = tk.Tk()
-		canvas = tk.Canvas(master, width=1500, height=1000)
-		canvas.pack()
-		for place in self.places:
-			canvas.create_oval(self.coordinates[place][0]+30, self.coordinates[place][1]+20, self.coordinates[place][0]-30, self.coordinates[place][1]-20)
-			canvas.create_text(self.coordinates[place][0],self.coordinates[place][1], text=place)
-
-		for trans in self.transitions:
-			canvas.create_rectangle(self.coordinates[trans][0]+30, self.coordinates[trans][1]+20, self.coordinates[trans][0]-30, self.coordinates[trans][1]-20)
-			canvas.create_text(self.coordinates[trans][0],self.coordinates[trans][1], text=trans)
-
-		for arc in self.arcs:
-			x1,y1,x2,y2 = 0,0,0,0
-			if arc[0]=='ptot':
-				x1,y1 = self.coordinates[arc[1]]
-				x2,y2 = self.coordinates[arc[2]]
-			else:
-				x1,y1 = self.coordinates[arc[2]]
-				x2,y2 = self.coordinates[arc[1]]
-
-			if x1+60<x2:
-				canvas.create_line(x1+30,y1,x2-30,y2,arrow=tk.LAST)
-			elif x1>x2+60 and y1<y2:
-				canvas.create_line(x1-30,y1,x2+30,y2,arrow=tk.LAST)
-			elif y1>y2:
-				canvas.create_line(x1,y1-30,x2,y2_30,arrow=tk.LAST)
-			else:
-				canvas.create_line(x1,y1+30,x2,y2-30,arrow=tk.LAST)
-
-			canvas.create_text((x1+x2)/2, (y1+y2)/2, text=arc[3])
-
-
-		tk.mainloop()
 
 
 class pdn:
+
+
 	def __init__(self):
 		self.places=[]
-		self.datums=[]
+		self.data=[]
 		self.transitions=[]
 		self.initMarking=dict()
-		## each transition will be a tuple with format(places, datums, input matrix, output matrix) with matrices as dict of dict
+		self.finalMarking=dict()
+		## each transition will be a tuple with format(places, data, input matrix, output matrix) with matrices as dict of dict
 
 
 	def addPlaces(self, listOfPlaces):
 		self.places = self.places + listOfPlaces
 
-	def addDatums(self, listOfDatums):
-		self.datums = self.datums = listOfDatums
+
+	def adddata(self, listOfdata):
+		self.data = self.data = listOfdata
+
 	
 	def addTrans(self, listOfTrans):
 		self.transitions = self.transitions + listOfTrans
+
 
 	#assume marking is a dict with items as list of ages 
 	def addInitMarking(self, marking):
 		self.initMarking = marking
 
+
+	def addFinalMarking(self,marking):
+		self.finalMarking = marking
+
+
 	def __str__(self):
+
+		#print places
 		val = "places: " + " ".join(self.places)+"\n"
-		val = val + "datums: " + " ".join(self.datums) + "\n"
+
+		#print data
+		val = val + "data: " + " ".join(self.data) + "\n"
+
+		#print transtions
 		for rule in self.transitions:
 			val = val + "Rule: \n"
 			val = val + "\tplaces: " + " ".join(rule[0]) + "\n" 
-			val = val + "\tdatums: " + " ".join(rule[1]) + "\n"
+			val = val + "\tdata: "
+			for datum in rule[1]:
+				val = val + "\t{:12}".format(datum)
+			val = val +"\n"
+
 			inputMat = rule[2]
 			outputMat = rule[3]
-
 			val = val + "\tinput: " + "\n"
 			for place in rule[0]:
-				val = val + "\t\t"
+				val = val + "\t    " +"{:12}".format(place)
 				for datum in rule[1]:
-					val = val + " " + str(inputMat[place][datum])
+					val = val + "\t" + '{:12}'.format(str(inputMat[place][datum]))
 				val = val + "\n"
 
 			val = val + "\toutput: " + "\n"
 			for place in rule[0]:
-				val = val + "\t\t"
+				val = val + "\t    "+ "{:12}".format(place)
 				for datum in rule[1]:
-					val = val + " " + str(outputMat[place][datum])
+					val = val + "\t" + '{:12}'.format(str(outputMat[place][datum]))
 				val = val + "\n"
 
+		#print marking
 		val = val + "Initial Markings: " + "\n"
 		count = 0
 		for place in self.initMarking.keys():
@@ -151,22 +147,15 @@ class pdn:
 
 		return val
 
+### END OF CLASS ###
+
 
 
 if __name__ == '__main__':	
-	net = tdpn()
-	net.addPlaces(["p1", "p2"])
-	net.addTrans(["tr1"])
-	coordinates = {"p1":(750,350), "p2":(750,650), "tr1":(750,500)}
-	arcs = [("ptot", "p1", "tr1", "[2,5]"),("ttop", "p2", "tr1", "[3,7]")]
-	net.addArcs(arcs)
-	net.addCoordinates(coordinates)
-	net.display()
-
-	# net.addDatums(["d1", "d2", "d3", "d4"])
-	# rules = []
-	# rules = rules + [(["p1", "p2"], ["d1", "d2", "d3"], {"p1":{"d1": 1, "d2": 1, "d3":0}, "p2": {"d1": 0, "d2": 1, "d3":1}}, {"p1": {"d1": 1, "d2": 0, "d3":1}, "p2": {"d1": 0, "d2": 0, "d3":1}})]
-	# net.addTrans(rules)
-	# print(net)
-
-
+	timedNet = tdpn()
+	timedNet.places = ["p","q","r","s"]
+	timedNet.data = ["X"]
+	timedNet.transitions = [("tr1", [("p", "closed", [1,2], None), ("q", "singular", [1], "X")], [("r", None, None, "X"), ("s", "closed", [1,2], None)])]
+	timedNet.initMarking = {"p":[0], "q": [1]}
+	timedNet.finalMarking = {"r":[2]}
+	print(timedNet)
--- timedToData.py
+++ timedToData.py
@@ -1,13 +1,359 @@
 from nets import tdpn, pdn
 import sys
+from itertools import product, permutations
+
+DEBUG=False;
+
+
+
+def divide(trans):
+	#helper function in the transition
+
+	inArcs = []
+	for arc in trans[1]:
+
+		lst = []
+		if arc[1] and arc[1] != "singular":
+
+			endpoints = arc[2]
+			for i in range(endpoints[0], endpoints[1]):
+				lst.append((arc[0], "singular", i, arc[3]))
+				lst.append((arc[0], "interval", i, arc[3]))
+
+			if arc[1] == "closed":
+				lst.append((arc[0], "singular", endpoints[1], arc[3]))
+
+			elif arc[1] == "leftOpen":
+				lst.append((arc[0], "singular", endpoints[1], arc[3]))
+				lst.pop(0)
+
+			elif arc[1] == "open":
+				lst.pop(0)
+
+
+		elif arc[1] and arc[1]=="singular":
+			lst = [(arc[0], "singlular", arc[2][0],arc[3])]
+
+
+		else:
+			lst = [arc[0], None, None, arc[3]]
+		inArcs.append(lst)
+
+
+	return [list(x) for x in  product(*inArcs)]
+
+### END OF HELPER FUNCTION "divide()" ####
+
+
+
+
+def translate(timedNet):
+
+	#intermediate step 1
+	newTransitions = []
+
+	for trans in timedNet.transitions:
+
+		inArc, outArc = trans[1],[]
+		counter = 1
+
+		for arc in trans[2]:
+
+			if arc[1] and arc[1]!="singular":
+				var = (trans[0] + ".X" + str(counter))
+				inArc.append(("storage", arc[1], arc[2],var))
+				outArc.append((arc[0], None, None, var))
+				timedNet.data.append(var)
+
+			else:
+				outArc.append(arc)
+
+		newTransitions.append((trans[0], inArc, outArc))
+	### END OF FOR LOOP ###
+
+	timedNet.places.extend(["storage", "infinity"])
+	timedNet.transitions = newTransitions + [("producer", [], [("storage", "singular",[0],None)])]
+	producerArcs = []
+
+	for place in timedNet.initMarking.keys():
+		producerArcs.append((place, "singular", [0], None))
+		timedNet.initMarking[place] = []
+	### END OF FOR LOOP ###
+
+
+	#intermediate step 2
+	timedNet.transitions = [item for sublist in list(map(lambda x: (list(map(lambda y: (x[0]+"."+str(y[0]+1),y[1],x[2]),enumerate(divide(x))))) , timedNet.transitions)) for item in sublist]
+	timedNet.transitions.append(("massProducer", [("infinity", None, None, None)], producerArcs))	
+	timedNet.initMarking["infinity"] = [1]
+
+
+	##ASERTION###
+	file = open("tests/first.test","r")
+	contents = file.read()
+	file.close()
+	assert str(timedNet)+"\n" == contents, '''Error in duplication'''
+
+
+	#Sorting
+	for item in timedNet.transitions:
+		arcs = item[1]
+		if arcs:
+			arcs.sort(reverse=True, key = lambda x: x[1])
+			arcs.sort(reverse=False, key = lambda x: x[2])
+	### END OF FOR LOOP ###
+
+
+	# declaring a new data net and finding maximal constant
+	dataNet = pdn()
+	maximal = 2
+
+
+	#defining places
+	newPlaces = ["int", "low", "high", "disc", "time0", "time1", "time2"]
+
+
+	## DEFINING TRANSITIONS ###
+
+	# Time elapse transitions 
+	#el0
+	inpMat = {"disc": {"el0.disc": 1, "el0.X":0, "el0.Y":0}, "high": {"el0.disc": 0, "el0.X":1, "el0.Y":0}, "time0": {"el0.disc": 0, "el0.X":0, "el0.Y":0}}
+	outMat = {"disc": {"el0.disc": 0, "el0.X":0, "el0.Y":0}, "high": {"el0.disc": 0, "el0.X":0, "el0.Y":1}, "time0": {"el0.disc": 1, "el0.X":0, "el0.Y":0}}
+	transitions = [(["disc","high","time0"], ["el0.disc","el0.X","el0.Y"], inpMat, outMat)]
+
+	#el1
+	inpMat = {"int":{"el1.disc": 0, "el1.X": 0, "el1.Y": 1}, "time0":{"el1.disc": 1, "el1.X": 0, "el1.Y": 0}, "time1":{"el1.disc": 0, "el1.X": 0, "el1.Y": 0}}
+	outMat = {"int":{"el1.disc": 0, "el1.X": 1, "el1.Y": 0}, "time0":{"el1.disc": 0, "el1.X": 0, "el1.Y": 0}, "time1":{"el1.disc": 1, "el1.X": 0, "el1.Y": 0}}
+	transitions.append((["time0","int","time1"],["el1.X","el1.Y","el1.disc"],inpMat, outMat))
+
+	#el2
+	inpMat = {"disc":{"el2.disc": 0}, "time1":{"el2.disc": 1}}
+	outMat = {"disc":{"el2.disc": 1}, "time1":{"el2.disc": 0}}
+	transitions.append((["disc", "time1"], ["el2.disc"], inpMat, outMat))
+
+	#el3
+	inpMat={"time1": {"el3.disc": 1}, "time2": {"el3.disc": 0}}
+	outMat={"time1": {"el3.disc": 0}, "time2": {"el3.disc": 1}}
+	transitions.append((["time1", "time2"], ["el3.disc"], inpMat, outMat))
+
+	#el4
+	inpMat={
+		"time2":{"el4.disc": 1,"el4.X": 0, "el4.Y": 0, "el4.Z": 0},
+		"low":{"el4.disc": 0,"el4.X": 1, "el4.Y": 0, "el4.Z": 0},
+		"disc": {"el4.disc": 0,"el4.X": 0, "el4.Y": 0, "el4.Z": 0},
+		"high": {"el4.disc": 0,"el4.X": 0, "el4.Y": 0, "el4.Z": 1}
+	}
+	outMat={
+		"time2":{"el4.disc": 0,"el4.X": 0, "el4.Y": 0, "el4.Z": 0},
+		"low":{"el4.disc": 0,"el4.X": 0, "el4.Y": 1, "el4.Z": 0},
+		"disc":{"el4.disc": 1,"el4.X": 0, "el4.Y": 0, "el4.Z": 0},
+		"high":{"el4.disc": 0,"el4.X": 0, "el4.Y": 0, "el4.Z": 1}
+	}
+	transitions.append((["time2", "low", "high", "disc"], ["el4.disc", "el4.X", "el4.Y", "el4.Z"], inpMat, outMat))
+
+
+	# Defining transitions tf.p.k, tf.p.max, ti.p.k for each place
+	for place in timedNet.places:
+
+		newPlaces = newPlaces + list(map(lambda x: place + "." + str(x), (list(range(0,maximal+1))+["inf"])))
+		#tf.p.k
+	
+		for k in range(maximal):
+			transName = "tr."+place+"."+str(k)
+			inpMat = {
+				"high":{transName+".X":0, transName+".Y":1, transName+".disc":0},
+				"time0":{transName+".X":0, transName+".Y":0, transName+".disc":1},
+				"int":{transName+".X":1, transName+".Y":0, transName+".disc":0},
+				(place+"."+str(k)):{transName+".X":1, transName+".Y":0, transName+".disc":0}
+			}
+			outMat = {
+				"high":{transName+".X":0, transName+".Y":1, transName+".disc":0},
+				"time0":{transName+".X":0, transName+".Y":0, transName+".disc":1},
+				"int":{transName+".X":1, transName+".Y":0, transName+".disc":0},
+				(place+"."+str(k)):{transName+".X":0, transName+".Y":1, transName+".disc":0}
+			}
+
+			transitions.append((["high","int","time0",place+"."+str(k)],[transName+x for x in [".X",".disc",".Y"]],inpMat,outMat))
+		### END OF FOR LOOP
+
+
+		#tf.p.max
+		transName = "tf."+place+"."+str(maximal)
+		inpMat = {
+			"time0":{transName+".disc":1, transName+".X":0},
+			"int":{transName+".disc":0, transName+".X":1},
+			place+"."+str(maximal):{transName+".disc":0, transName+".X":1},
+			place+".inf":{transName+".disc":0, transName+".X":0}
+		}
+		outMat = {
+			"time0":{transName+".disc":1, transName+".X":0},
+			"int":{transName+".disc":0, transName+".X":1},
+			place+"."+str(maximal):{transName+".disc":0, transName+".X":0},
+			place+".inf":{transName+".disc":1, transName+".X":0}
+		}
+		transitions.append((["time0","int",place+"."+str(maximal), place+".inf"],[transName+x for x in [".X",".disc"]],inpMat,outMat))
+
+
+		#ti.p.k
+		for k in range(maximal):
+			transName = "ti."+place+str(k)
+			inpMat = {
+				"time2":{transName+".X": 0, transName+".Y": 0,transName+".disc": 1},
+				"low":	{transName+".X": 0, transName+".Y": 1,transName+".disc": 0},
+				"int": {transName+".X": 1, transName+".Y": 0,transName+".disc": 0},
+				place+"."+str(k): {transName+".X": 0, transName+".Y": 1,transName+".disc": 0},
+				place+"."+str(k+1): {transName+".X": 0, transName+".Y": 0,transName+".disc": 0}
+			}
+			outMat = {
+				"time2":{transName+".X": 0, transName+".Y": 0,transName+".disc": 1},
+				"low":	{transName+".X": 0, transName+".Y": 1,transName+".disc": 0},
+				"int": {transName+".X": 1, transName+".Y": 0,transName+".disc": 0},
+				place+"."+str(k): {transName+".X": 0, transName+".Y": 0,transName+".disc": 0},
+				place+"."+str(k+1): {transName+".X": 1, transName+".Y": 0,transName+".disc": 0}
+			}
+			transitions.append((["time2","low","int",place+"."+str(k),place+"."+str(k+1)],[transName+x for x in [".X",".disc",".Y"]],inpMat,outMat))
+		### END OF FOR LOOP ####
+
+
+	## DISCRETE TRANSTIONS ###
+
+	lst = []
+	for trans in timedNet.transitions:
+
+		inpMat = dict()
+		outMat = dict()
+		data = []
+		places = ["int", "low"]
+		buff = -1
+		temp = None
+		num = 0
+
+		for arc in trans[1]:
+			if arc:
+				datum = None
+				if arc[1]!=temp:
+					temp = arc[1]
+					buff = arc[2]
+					data.append([])
+					if not arc[3]:
+						num += 1
+						datum = trans[0] + ".XD" + str(num)
+					else:
+						datum = arc[3]
+					data[-1].append(datum)
+
+				elif arc[1] == temp and arc[1]=="singular" and buff==arc[2]:
+					data.append([])
+					if not arc[3]:
+						datum = trans[0] + ".XD" + str(num)
+					else:
+						datum = arc[3]
+					data[-1].append(datum)
+
+				elif arc[1] == temp and arc[1]=="singular" and buff!=arc[2]:
+					buff = arc[2]
+					data.append([])
+					if not arc[3]:
+						num += 1 
+						datum = trans[0] + ".XD" + str(num)
+					else:
+						datum = arc[3]
+					data[-1].append(datum)
+
+				elif arc[1] == temp and arc[1]=="interval" and buff == arc[2]:
+					if not arc[3]:
+						num += 1
+						datum = trans[0] + ".XD" + str(num)
+					else:
+						datum = arc[3]
+					data[-1].append(datum)
+
+				elif arc[1] == temp and arc[1]=="interval" and buff != arc[2]:
+					buff = arc[2]
+					data.append([])
+					if not arc[3]:
+						num += 1
+						datum = trans[0] +".XD"+str(num)
+		
+					else:
+						datum = arc[3]
+						data[-1].append(datum)
+
+				else:
+					pass
+
+				src = arc[0] + "." + str(arc[2])
+				places.append(src)
+				inpMat.setdefault(src,{})[datum] = 1
+		### END of FOR LOOP ###
+		
+		for arc in trans[2]:
+			if arc:
+				dst = None
+				iden = None
+				try:
+					if arc[3] == None:
+						dst = arc[0]+"."+arc[2][0]
+						iden = trans[0]+".int"
+					else:
+						for arc1 in trans[1]:
+							if arc1:
+								if arc1[3] == arc[3]:
+									dst = arc[0]+"."+str(arc1[2])
+									iden = arc[3]
+					places.append(dst)
+					outMat.setdefault(dst,{})[iden]=1
+
+				except:
+					pass
+		### END OF FOR LOOP ###		
+			
+
+		inpMat.setdefault("disc",{})[trans[0]+".disc"]=1
+		inpMat.setdefault("int",{})[trans[0]+".int"]=1
+		inpMat.setdefault("low",{})[trans[0]+".low"]=1
+
+		outMat.setdefault("disc",{})[trans[0]+".disc"]=1
+		outMat.setdefault("int",{})[trans[0]+".int"]=1
+		outMat.setdefault("low",{})[trans[0]+".low"]=1
+
+
+		L = [list(permutations(x)) for x in data]
+		data =[[item for sub in sublist for item in sub] for sublist in list(product(*L))] 
+
+		for arrange in data:
+			for place in places:
+
+				inpMat.setdefault(place,{})
+				outMat.setdefault(place,{})
+
+				for datum in [trans[0]+".int",trans[0]+".low"]+arrange:
+
+					inpMat[place].setdefault(datum,0)
+					outMat[place].setdefault(datum,0)
+				### END OF FOR LOOP ###		
+			### END OF FOR LOOP ###		
+			lst.append((places, [trans[0]+".int",trans[0]+".low"]+arrange, inpMat, outMat))
+		### END OF FOR LOOP ###
+
+	transitions.extend(lst)
+	dataNet.addPlaces(newPlaces)
+	dataNet.addTrans(transitions)
+	return dataNet
+#### END OF FUNCTION "translate" ###
+
+
+
 def main(argv):
 	timedNet = tdpn()
-	timedNet.readFromCPN(argv[1])
+	timedNet.places = ["p","q","r","s"]
+	timedNet.data = ["X"]
+	timedNet.transitions = [("tr1", [("p", "open", [1,2], None), ("q", "singular", [1], "X")], [("r", None, None, "X"), ("s", "open", [1,2], None)])]
+	timedNet.initMarking = {"p":[0], "q": [1]}
+	timedNet.finalMarking = {"r":[2]}
+	d = translate(timedNet)
+	print(d)
 
-	print(timedNet)
 
-
-	print(dataNet)
 
 if __name__ == '__main__':
 	main(sys.argv)
